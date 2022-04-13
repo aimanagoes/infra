0d7ae9a453efb50bf166469c83361fa7f246bb5b
aimanagoes <99529123+aimanagoes@users.noreply.github.com>
2022-04-14 01:00:55 +0800

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password@123" -e "MSSQL_PID=Express" -p 1433:1433 -v mssqlserver_volume:/var/opt/mssql --name mssql -d mcr.microsoft.com/mssql/server:2019-CU15-ubuntu-20.04
 docker ps
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password@123
+docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password@123 <<!
 create database Enovade
 go
 select name from sys.databases
@@ -17,3 +17,4 @@ go
 select * from pengguna
 go
 exit
+!

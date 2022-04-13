ddb7d698e3a2b9eb63014ae5af13a9c54c91ab22
aimanagoes <99529123+aimanagoes@users.noreply.github.com>
2022-04-13 23:19:01 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -7,10 +7,11 @@ sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password@123" -e "MSSQL_PID=Express" -p 1433:1433 -v mssqlserver_volume:/var/opt/mssql --name mssql -d mcr.microsoft.com/mssql/server:2019-CU15-ubuntu-20.04
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs -y
+sudo apt install -y nodejs
 sudo apt install build-essential -y
 npm install pm2 -g
 sudo apt install php-cli unzip -y
 cd ~
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo apt install phpunit -y
